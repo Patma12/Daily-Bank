e38cfdfd7cafbdc4c76a85b029a643f671e06b5f
Patma12 <patmaforbs@gmail.com>
2024-07-03 17:14:03 +0700

    update for phon responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      's1': {'max': '400px'},}
   },
   plugins: [
     require("@tailwindcss/forms"),
