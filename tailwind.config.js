e9bccf6ada57e4ce36c0a3813490ee9d237cf18a
Patma12 <patmaforbs@gmail.com>
2024-07-04 12:27:01 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/*.{html,js}"],
   theme: {
     extend: {},
     screens: {
-      's1': {'max': '400px'},}
+      s1: { max: "400px" },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
